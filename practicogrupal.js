3de1961314ad0f69109058354d5d5db1590e341f
Manuela Barbera <manuelabarbera@MacBook-Air-de-Manuela.local>
2019-10-25 20:43:14 -0300

    first commit

---
+++ practicogrupal.js
@@ -0,0 +1,25 @@
+
+const nombreDeLaTienda = prompt(`Bienvenido a: Tu vieja en tanga 👙 
+Ingresá tu nombre:`)
+
+alert (` 📣 Hola, tenemos en oferta los siguientes productos:
+♠️ Tanga
+🖤 Culotte`)
+
+const comprarTanga = prompt("Desea comprar ♠️ tanga?")
+
+const comprarCulotte = prompt("Desea comprar 🖤 culotte?")
+
+const abonarConTarjeta = prompt(`🧾 El detalle de tu compra es:
+♠️ Tanga $ por unidad
+🖤 Culotte $ por unidad
+💵 Total: $
+💳 ¿Desea abonar con tarjeta?`)
+
+const codigoDeDescuento = prompt(`🧾 El detalle de tu compra es:
+♠️ Tanga 
+🖤 Culotte
+💵 Total: $
+💳 ¿Tenés un código de descuento?`) 
+
+
