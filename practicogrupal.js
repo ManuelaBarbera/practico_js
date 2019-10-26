83cc4f7699009f160e9734942d78af8efccb02ad
Manuela Barbera <manuelabarbera@MacBook-Air-de-Manuela.local>
2019-10-25 22:13:52 -0300

    actualizacion

--- practicogrupal.js
+++ practicogrupal.js
@@ -1,3 +1,5 @@
+let Tanga = 100;
+let Culotte = 200; 
 
 const nombreDeLaTienda = prompt(`Bienvenido a: Tu vieja en tanga 👙 
 Ingresá tu nombre:`)
@@ -6,20 +8,26 @@ alert (` 📣 Hola, tenemos en oferta los siguientes productos:
 ♠️ Tanga
 🖤 Culotte`)
 
-const comprarTanga = prompt("Desea comprar ♠️ tanga?")
+const comprarTanga = prompt("Desea comprar ♠️ tanga?");
+let cantidadTanga = comprarTanga == "SI" && prompt("que cantidad desea comprar?");
 
-const comprarCulotte = prompt("Desea comprar 🖤 culotte?")
+const comprarCulotte = prompt("Desea comprar 🖤 culotte?");
+let cantidadCulotte = comprarCulotte == "SI" && prompt("que cantidad desea comprar?");
 
 const abonarConTarjeta = prompt(`🧾 El detalle de tu compra es:
-♠️ Tanga $ por unidad
-🖤 Culotte $ por unidad
-💵 Total: $
+♠️ Tanga: ${cantidadTanga} x $100
+🖤 Culotte: ${cantidadCulotte} x $200
+💵 Total: ${(cantidadTanga * Tanga) + (cantidadCulotte * Culotte)}
 💳 ¿Desea abonar con tarjeta?`)
 
+const cuantasCuotas = prompt("En cuantas cuotas desea abonarlo?")
+
 const codigoDeDescuento = prompt(`🧾 El detalle de tu compra es:
-♠️ Tanga 
-🖤 Culotte
-💵 Total: $
+♠️ Tanga: ${cantidadTanga} x $100
+🖤 Culotte: ${cantidadCulotte} x $200
+💵 Total: ${(cantidadTanga * Tanga) + (cantidadCulotte * Culotte)}
+A pagar en ${cuantasCuotas} cuotas de ${((cantidadTanga * Tanga) + (cantidadCulotte * Culotte)) / (cuantasCuotas)} pesos
 💳 ¿Tenés un código de descuento?`) 
+
 
 
